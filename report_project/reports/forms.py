4b591a48eedb9cce56baa624200e705295c9a978
KYD <kydkang@gmail.com>
2019-09-30 10:17:48 -0500

    20190930-moveBackTheGrafico

--- report_project/reports/forms.py
+++ report_project/reports/forms.py
@@ -4,13 +4,30 @@ from .models import Informe, SatImage
 class InformeForm(forms.ModelForm):
     class Meta:
         model = Informe
+        # template_name = 'reports/informe_form.html' 
         fields = ['event', 'satimage1', 'satimage2', 'informe_code', 'title', 'fecha']
-        ### also need change in settings.py to change the order to 25-Enero-2019
-        ###  USE_L10N = False    DATE_FORMAT = '%d %m %Y'  LANGUAGE_CODE = 'es-EC'
+        ### also need change as follows in settings.py to change the order to 25-Enero-2019
+        ###  USE_L10N = False    DATE_FORMAT = 'd-m-Y'  LANGUAGE_CODE = 'es-EC'
         widgets = {
             'fecha': forms.SelectDateWidget
         }
-        # template_name = 'reports/informe_form.html' 
+
+        labels = {
+            'event': 'Evento',
+            'satimage1': 'Imagen Satelital 1', 
+            'satimage2': 'Imagen Satelital 2',
+            'informe_code': 'Informe Code',
+            'title': 'Titulo',
+            'fecha': 'Fecha',
+        }
+        # help_texts = {
+        #     'name': _('Some useful help text.'),
+        # }
+        # error_messages = {  
+        #     'name': {
+        #         'max_length': _("This writer's name is too long."),
+        #     },
+        # }
 
 
     def __init__(self,*args, **kwargs):
@@ -19,14 +36,14 @@ class InformeForm(forms.ModelForm):
         self.fields['satimage1'].queryset=SatImage.objects.none()
         self.fields['satimage2'].queryset=SatImage.objects.none() 
 
-        if 'event' in self.data: 
+        if 'event' in self.data:     ## handling ajax request 
             try:
                 event_id = int(self.data.get('event'))
                 self.fields['satimage1'].queryset = SatImage.objects.filter(event_id=event_id).order_by('fecha')
                 self.fields['satimage2'].queryset = SatImage.objects.filter(event_id=event_id).order_by('fecha')
             except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty City queryset
-        elif self.instance.pk:    # if there is an instance in the form
+        elif self.instance.pk:    # if there is an form intance.  satimage_set is the reverse set for SatImage
             self.fields['satimage1'].queryset = self.instance.event.satimage_set.order_by('fecha')
             self.fields['satimage2'].queryset = self.instance.event.satimage_set.order_by('fecha') 
 
--- report_project/reports/models.py
+++ report_project/reports/models.py
@@ -4,17 +4,17 @@ from django.urls import reverse
 class Event(models.Model): 
     dmeva_code  = models.CharField(max_length=20) 
     dmeva_fecha = models.DateField()
-    mapa        = models.ImageField(upload_to='mapa/')
-    # mapa        = models.PolygonField(upload_to='mapa/')
+    mapa        = models.ImageField(upload_to='mapa/')     ### models.PolygonField(upload_to='mapa/')
+    grafico     = models.ImageField(upload_to='grafico/')
     def __str__(self): 
         return self.dmeva_code  
 
 class Informe(models.Model):
     event       = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
-    satimage1   = models.ForeignKey('SatImage', null=True, on_delete=models.SET_NULL, related_name='informe1')
-    satimage2   = models.ForeignKey('SatImage', null=True, on_delete=models.SET_NULL, related_name='informe2') 
+    satimage1   = models.ForeignKey('SatImage', null=True, on_delete=models.SET_NULL, related_name='informe1', verbose_name='Imagen Satelital 1')
+    satimage2   = models.ForeignKey('SatImage', null=True, on_delete=models.SET_NULL, related_name='informe2', verbose_name='Imagen Satelital 2') 
     informe_code= models.CharField(max_length=20)
-    title       = models.CharField(max_length=200)
+    title       = models.CharField(max_length=200, verbose_name='Titulo')
     fecha       = models.DateField()
     def __str__(self): 
         return self.informe_code  
@@ -23,32 +23,32 @@ class Informe(models.Model):
 
 
 class SatImage(models.Model): 
-    event       = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event       = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Evento')
     fuente      = models.CharField(max_length=50)
     banda       = models.CharField(max_length=50)
     fecha       = models.DateField()
     # antes       = models.BooleanField()
-    image       = models.ImageField(upload_to='satimages')  
+    image       = models.ImageField(upload_to='satimages', verbose_name='Imagen')  
     def __str__(self): 
         return str(self.id)  
 
 class Area(models.Model):   
-    event       = models.ForeignKey(Event, on_delete=models.CASCADE)
-    location    = models.ForeignKey('Location', on_delete=models.CASCADE)  
+    event       = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Evento')
+    location    = models.ForeignKey('Location', on_delete=models.CASCADE, verbose_name='Localización')  
     superficie  = models.DecimalField(max_digits=8, decimal_places=2)
-    hectarea    = models.DecimalField(max_digits=8, decimal_places=2)
-    percentage  = models.DecimalField(max_digits=5, decimal_places=2)
-    grafico     = models.ImageField(upload_to='grafico/')
+    hectarea    = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Hectáreas')
+    percentage  = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Porcentaje')
+
     def __str__(self): 
         return str(self.id)
 
 class Location(models.Model):  
-    locationID  = models.CharField(max_length=6, primary_key=True) 
+    location_code  = models.CharField(max_length=6, primary_key=True, verbose_name='Localización Código') 
     province    = models.CharField(max_length=50)
     canton      = models.CharField(max_length=50)
     parroquia   = models.CharField(max_length=50)
     def __str__(self): 
-        return self.locationID
+        return self.location_code
 
 # class Provincia(models.Model): 
 #     name        = models.CharField(max_length=30)
